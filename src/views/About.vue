cb3381c40fb90faf96a469b4ed27c55f84656451
daniel.abajirov <abajirovdaniel@gmail.com>
2021-07-30 16:14:53 +0200

    Adding new theo app

---
+++ src/views/About.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="about">
+    <TestQuestions/>
+  </div>
+</template>
+
+
+<script>
+// @ is an alias to /src
+
+import TestQuestions from '../components/TestQuestions.vue'
+
+export default {
+  name: 'About',
+  components: {
+    TestQuestions
+
+    
+  }
+}
+</script>
+
