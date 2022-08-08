82c9c71c4364ffa6230b47c248dcad0a25df87b3
daniel.abajirov <abajirovdaniel@gmail.com>
2022-08-08 17:44:05 +0200

    doc: add new image

--- src/firebase.js
+++ src/firebase.js
@@ -3,7 +3,6 @@ import 'firebase/database'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.VUE_APP_APIKEY,
   authDomain: process.env.VUE_APP_AUTHDOMAIN,
   databaseURL: process.env.VUE_APP_DATABASEURL,
   projectId: process.env.VUE_APP_PROJECTID,
