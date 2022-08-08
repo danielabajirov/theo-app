d19738c0812fd4485251ce58d38b8af7471ef9ca
daniel.abajirov <abajirovdaniel@gmail.com>
2022-08-08 12:59:51 +0200

    fix: fix some errors

--- src/firebase.js
+++ src/firebase.js
@@ -4,7 +4,6 @@ import 'firebase/database'
 // Your web app's Firebase configuration
 // Your web app's Firebase configuration
 console.log("Test", process.env)
-console.log("Test", process)
 console.log("Test", process.env.APIKEY)
 console.log("Test", process.env.DATABASEURL)
 const firebaseConfig = {
