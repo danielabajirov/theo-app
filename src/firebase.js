041c0465de5bb6b8df1918e8588f9bffcc34fec4
daniel.abajirov <abajirovdaniel@gmail.com>
2022-08-08 13:13:30 +0200

    fix: fix some errors

--- src/firebase.js
+++ src/firebase.js
@@ -2,24 +2,19 @@ import firebase from "firebase/app";
 import 'firebase/database'
 
 // Your web app's Firebase configuration
-// Your web app's Firebase configuration
-console.log("Test", process.env)
-console.log("Test", process.env.APIKEY)
-console.log("Test", process.env.THEO_APP_APIKEY)
 const firebaseConfig = {
-  apiKey: process.env.APIKEY,
-  authDomain: process.env.AUTHDOMAIN,
-  databaseURL: process.env.DATABASEURL,
-  projectId: process.env.PROJECTID,
-  storageBucket: process.env.STORAGEBUCKET,
-  messagingSenderId: process.env.MESSAGINGSENDERID,
-  appId: process.env.APPID,
-  measurementId: process.env.MEASUREMENTID
+  apiKey: process.env.VUE_APP_APIKEY,
+  authDomain: process.env.VUE_APP_AUTHDOMAIN,
+  databaseURL: process.env.VUE_APP_DATABASEURL,
+  projectId: process.env.VUE_APP_PROJECTID,
+  storageBucket: process.env.VUE_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.VUE_APP_MESSAGINGSENDERID,
+  appId: process.env.VUE_APP_APPID,
+  measurementId: process.env.VUE_APP_MEASUREMENTID
 };
 
   firebase.initializeApp(firebaseConfig);
   const db = firebase.database();
-  console.log("this db", db);
 
   export default db;
 
